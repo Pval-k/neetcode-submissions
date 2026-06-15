3c84528b708bcc9066bc38ae3afe78bfd3eb553d
Pval-k <97149043+Pval-k@users.noreply.github.com>
2026-06-15 14:36:07 -0700

    Add: two-integer-sum - submission-17

---
+++ Data Structures & Algorithms/two-integer-sum/submission-17.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        //key: complement
+        //value: index
+        HashMap<Integer,Integer> hash = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            int complement = target - nums[i];
+
+            if(hash.containsKey(complement)){
+                return new int[] {hash.get(complement), i};
+            }
+            else{
+                hash.put(nums[i], i);
+            }
+        }
+        return new int[] {};
+    }
+}
