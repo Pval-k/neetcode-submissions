41a7d97e68de427a366b68bb67a8b920952a886a
Pval-k <97149043+Pval-k@users.noreply.github.com>
2026-06-22 14:25:10 -0700

    Add: is-palindrome - submission-14

---
+++ Data Structures & Algorithms/is-palindrome/submission-14.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+
+        s = s.toLowerCase();
+        
+        int i = 0;
+        int j = s.length()-1;
+
+        while(i<j){
+            
+            while(i<j && !Character.isLetterOrDigit(s.charAt(j))){
+                j--;
+            }
+
+            while(i<j && !Character.isLetterOrDigit(s.charAt(i))){
+                i++;
+            }
+
+            if(s.charAt(i) == s.charAt(j)){
+                i++;
+                j--;
+            }
+            else{
+                return false;
+            }
+
+        }
+
+        return true;
+        
+    }
+}
