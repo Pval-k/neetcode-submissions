e729ee6b138a4ff1e17b250a253ed76edef25d2a
Pravalhika Kurapati <97149043+Pval-k@users.noreply.github.com>
2026-07-05 15:54:31 -0700

    Add: permutation-string - submission-5

---
+++ Data Structures & Algorithms/permutation-string/submission-5.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        char[] s1_arr = s1.toCharArray();
+        Arrays.sort(s1_arr);
+
+        int window_size = s1.length();
+        int i = 0;
+        int j = i+window_size-1;
+        while(j<s2.length()){
+            char[] s2_window = s2.substring(i,j+1).toCharArray();
+            Arrays.sort(s2_window);
+
+            if(java.util.Arrays.equals(s1_arr, s2_window)){
+                return true;
+            }
+            i++;
+            j++;
+        }
+        return false;
+    }
+}
