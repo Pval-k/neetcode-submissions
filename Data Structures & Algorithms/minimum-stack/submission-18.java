37009bc4ec521c6bd80696dbf1d3b7f6233e8ee9
Pravalhika Kurapati <97149043+Pval-k@users.noreply.github.com>
2026-09-10 11:57:01 -0700

    Add: minimum-stack - submission-18

---
+++ Data Structures & Algorithms/minimum-stack/submission-18.java
@@ -0,0 +1,33 @@
+class MinStack {
+    Stack<Integer> stack;
+    Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+    
+    public void push(int val) {
+        if(minStack.isEmpty() || minStack.peek() >= val){
+            minStack.push(val);
+        }
+        stack.push(val);
+
+    }
+    
+    public void pop() {
+        if(stack.peek().equals(minStack.peek())){
+            minStack.pop();
+        }
+        stack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+        
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
